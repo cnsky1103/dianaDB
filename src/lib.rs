d145b4ed0169c0a443dd70da3f38f50ff56cdda0
cnsky1103 <cnsky1103@gmail.com>
2023-07-03 19:03:25 -0700

    datatype and expression

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![allow(non_snake_case)]
+#![warn(clippy::all)]
+#![allow(clippy::new_without_default)]
+#![allow(clippy::unneeded_field_pattern)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod error;
+pub mod sql;
